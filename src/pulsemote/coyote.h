fd421a47a9cc0235b157beabf0842eb161e96101
JLatex <jlatex@gmail.com>
2023-11-02 09:08:55 +0000

    Testing showed don't start at 15%, start at 0

--- src/pulsemote/coyote.h
+++ src/pulsemote/coyote.h
@@ -1,4 +1,4 @@
 int coyote_ax = 0; int coyote_ay = 0; int coyote_az = 0;
 int coyote_bx = 0; int coyote_by = 0; int coyote_bz = 0;
-#define start_powerA 300 // max is usually 2000
-#define start_powerB 300 // max is usually 2000
+#define start_powerA 0 // max is usually 2000
+#define start_powerB 0 // max is usually 2000
